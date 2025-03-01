d177201b69d9c4212ac27b8762921cb29b34fdbf
sruubka <166057134+sruubka@users.noreply.github.com>
2025-03-01 16:53:19 +0100

    Update chatgpt.lua

--- chatgpt.lua
+++ chatgpt.lua
@@ -58,7 +58,7 @@ UpdatePlayerList()
 
 -- Fly system
 local flying = false
-local flySpeed = 5  -- prędkość lotu
+local flySpeed = 50  -- prędkość lotu
 local flyVelocity = Vector3.new()
 
 local keys = {W = 0, A = 0, S = 0, D = 0, Space = 0, LeftShift = 0}
